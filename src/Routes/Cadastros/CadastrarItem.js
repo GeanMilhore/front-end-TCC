38e2c7488c790c17dd82bb12f2e5c3395b9f7455
GeanMilhore <gean.milhore@gmail.com>
2021-08-21 03:52:02 -0300

    funcionalidade imagem

--- src/Routes/Cadastros/CadastrarItem.js
+++ src/Routes/Cadastros/CadastrarItem.js
@@ -4,18 +4,39 @@ import style from "./CadastrarItem.module.css";
 import Input from "../../Components/Smart-components/Input/Input.js";
 import Switch from "../../Components/Smart-components/Switch/Switch";
 import imageItem from "../../resources/images/add-item-image.png";
+import axios from "axios";
 
 const CadastrarItem = () => {
   const [nome, setNome] = React.useState("");
   const [checked, setChecked] = React.useState(false);
   const [qtd, setQtd] = React.useState(1);
   const [desc, setDesc] = React.useState("");
+  const [img, setImg] = React.useState(null);
+  const [preview, setPreview] = React.useState(null);
+  const inputFile = React.useRef();
 
   React.useEffect(() => {
     if (checked) {
       setQtd(1);
     }
   }, [checked]);
+
+  function fileSelectedHandler(event) {
+    setImg(event.target.files[0]);
+    if (event.target.files && event.target.files[0]) {
+      var file = new FileReader();
+      file.onload = function (e) {
+        setPreview(e.target.result);
+      };
+      file.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  function submitForm() {
+    const fd = new FormData();
+    fd.append("image", img, img.name);
+    axios.post("--url api");
+  }
 
   return (
     <div className="container-middle">
@@ -43,22 +64,26 @@ const CadastrarItem = () => {
                 value={nome}
                 setValue={setNome}
               />
-              <Switch
-                id="unico"
-                label="Item único:"
-                setValue={setChecked}
-                buttonclass={style.switchButton}
-                labelclass={style.labelClass}
-              />
-              <Input
-                id="qtd"
-                label="Quantidade:"
-                type="number"
-                value={qtd}
-                setValue={setQtd}
-                disabled={checked}
-                min={1}
-              />
+              <div className={style.divUnicQtd}>
+                <Switch
+                  id="unico"
+                  label="Item único:"
+                  setValue={setChecked}
+                  buttonclass={style.switchButton}
+                  labelclass={style.labelClass}
+                />
+                <Input
+                  id="qtd"
+                  label="Quantidade:"
+                  type="number"
+                  value={qtd}
+                  setValue={setQtd}
+                  disabled={checked}
+                  min={1}
+                  className={style.divUnicQtd}
+                />
+              </div>
+
               <Input
                 id="desc"
                 label="Descrição:"
@@ -67,8 +92,18 @@ const CadastrarItem = () => {
                 setValue={setDesc}
               />
             </form>
-            <div className={style.imgItem}>
-              <img src={imageItem} alt="símbolo de galeria" />
+            <div className={style.imgItem} onClick={() => inputFile.current.click()}>
+              <div className={style.imgAlterar}>{img ? 'Alterar Imagem' : 'Adicionar Imagem'}</div>
+              <input
+                type="file"
+                onChange={fileSelectedHandler}
+                style={{ display: "none" }}
+                ref={inputFile}
+              />
+              <img
+                src={preview ? preview : imageItem}
+                alt="símbolo de galeria"
+              />
             </div>
           </div>
         </Modal.Body>
