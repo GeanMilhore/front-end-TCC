a113d5ed730789c520a4123aee7a3931134b68fc
GeanMilhore <gean.milhore@gmail.com>
2021-12-02 01:18:34 -0300

    remoção de componentes antigos

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ListaContas from "./ListaContas/ListaContas";
-import ListaInteresses from "./ListaInteresses/ListaInteresses";
 import NavHome from "./NavHome/NavHome";
 import style from './Home.module.css'
 
@@ -13,7 +12,6 @@ const Home = ({ links, endPoints, label, imgs, mensagens }) => {
       <div className={style.home}>
         <Routes>
           <Route path={links[0]} element={<ListaContas endPoint={endPoints[0]} />} />
-          <Route path={links[1]} element={<ListaInteresses endPoint={endPoints[1]} />} />
         </Routes>
       </div>
     </>
